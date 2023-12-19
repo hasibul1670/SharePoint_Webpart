453747ac7da50cf351675ad5b7e901f75cdf7f3c
Hasibul Islam <hasibulislam1670@gmail.com>
2023-12-19 17:25:48 +0600

    init :: SharePoint Project Setup Done.

---
+++ src/webparts/employeeForm/components/IEmployeeFormProps.ts
@@ -0,0 +1,7 @@
+export interface IEmployeeFormProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/employeeForm/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IEmployeeFormWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'EmployeeFormWebPartStrings' {
+  const strings: IEmployeeFormWebPartStrings;
+  export = strings;
+}
